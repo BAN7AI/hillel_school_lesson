ee074a326a362737a32423bc4596fd6a80b693ca
BAN7AI <vadym.sokolov1992@gmail.com>
2024-04-25 12:32:22 +0300

    edit file

--- first_lesson.py
+++ first_lesson.py
@@ -1,1 +1,24 @@
-print ("Hello world!")
+# sum of two numbers
+result = 1 + 1 # TODO calculation
+print("Result", result)
+print("Result", type("Result"))
+print(result, type(result))
+
+number = 2
+string = "2"
+
+number_a = 7    #straight method
+numbet_b = number_a #reference to another object
+print(numbet_b, number_a)
+
+number_a, number_b = 4, 12 + 3   #multiple assignment exist, not recommended
+print(number_a, number_b)
+
+#type() - вычисляет тип данных и сообщает
+#value:str = "string" - анатация типа к имени объекта (в данном случае строка (
+#             value:str = "string", value: int = 12))
+#id() - встроенная функция, который возвращает идитификатор объекта
+
+my_first_value = 3
+print(id(my_first_value), id(2), id(3))
+
