883c873df61138c3ed7e3d98cc161a80fc0eeb90
BAN7AI <vadym.sokolov1992@gmail.com>
2024-04-26 11:35:17 +0300

    homework 2, lesson 2

---
+++ rows_and_numbers.py
@@ -0,0 +1,12 @@
+# first
+name_user: str = input("Hello please write me how your name: ")
+print(f"Hello {name_user}. Nice too meet you. My name BOBER")
+
+# second
+
+float_value: float = float(input("Hello user, write me float number here: "))
+print("Your number -", float_value)
+print(int(float_value))
+print(float_value ** 4)
+print(float_value * 0.5)
+print(float_value % 2)
