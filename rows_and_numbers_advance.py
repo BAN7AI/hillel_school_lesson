08c1c8d7be053edfcde05ad3a9f73655b719ffa6
BAN7AI <vadym.sokolov1992@gmail.com>
2024-04-26 11:58:31 +0300

    edit PEP-8 in correct

--- rows_and_numbers_advance.py
+++ rows_and_numbers_advance.py
@@ -2,18 +2,18 @@
 name_user = input("Hello user, write your name here please: ")
 print(name_user)
 
-#second
+# second
 
-#print(name_user.replace(" ", ""))  #удаляет все пробелы или другие знаки которые указать в old
-name_user = name_user.strip()  #удаляет пробелы спереди и сзади
+# print(name_user.replace(" ", ""))  # удаляет все пробелы или другие знаки которые указать в old
+name_user = name_user.strip()  # удаляет пробелы спереди и сзади
 print(name_user)
 
-#third
-print(f"Hello user, {name_user.title()}")  #Пишит первую букву заглавную последующие маленькие
+# third
+print(f"Hello user, {name_user.title()}")  # Пишит первую букву заглавную последующие маленькие
 
-#fourd
-print(f"Len your name: {len(name_user)}")  #кол-во букв в строке
+# fourd
+print(f"Len your name: {len(name_user)}")  # кол-во букв в строке
 
-#five
-print("".join(reversed(name_user)))  #Друкує ім'я задом наперед
-print(name_user[::-1])  #Друкує ім'я задом наперед
+# five
+print("".join(reversed(name_user)))  # Друкує ім'я задом наперед
+print(name_user[::-1])  # Друкує ім'я задом наперед
