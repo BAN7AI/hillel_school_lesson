64f4c678181cb30771b15ed138cbca2007586ee6
BAN7AI <vadym.sokolov1992@gmail.com>
2024-04-26 11:19:24 +0300

    homework1(lesson 2)

---
+++ data_types.py
@@ -0,0 +1,21 @@
+one_value = "Hello world!"
+two_value = "New message"
+three_value = "why not?"
+four_value = 12
+five_value = 12.2
+six_value = 12 + 13
+seven_value = 12 - 1
+eight_value = "I don't know, what it's need write here"
+nine_value = 12.2 - 35.3
+ten_value = input("Write here your message and tap the Enter on your keyboard: ")
+
+print(one_value, type(one_value))
+print(two_value, type(two_value))
+print(three_value, type(three_value))
+print(four_value, type(four_value))
+print(five_value, type(five_value))
+print(six_value, type(six_value))
+print(seven_value, type(seven_value))
+print(eight_value, type(eight_value))
+print(nine_value, type(nine_value))
+print(ten_value, type(ten_value))
