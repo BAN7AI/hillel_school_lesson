11ae313efe7d94b9a60328c3d027e07131a13c08
BAN7AI <vadym.sokolov1992@gmail.com>
2024-04-29 11:39:08 +0300

    edit file

---
+++ boll_type.py
@@ -0,0 +1,23 @@
+print(3 != 5)
+
+#######################################################
+
+print(5 == 5)
+print(5 >= 5)
+print(5 <= 5)
+
+#######################################################
+
+print(True or True or False)
+print((True and True) or False)
+print(True or (True and False))
+print(not (True and True and False))
+print(not (True and True and False))
+
+#######################################################
+
+print(bool(None == 7))
+print(bool("" == 10 - 7))
+str_value = "Test message"
+print(bool(True or False == str_value))
+print(bool(id(None) == type(None)))
