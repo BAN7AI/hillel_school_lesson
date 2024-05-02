43d958800c346103f76ace0fdf655a08e180cc52
BAN7AI <vadym.sokolov1992@gmail.com>
2024-05-02 14:14:31 +0300

    edit homework

--- loops.py
+++ loops.py
@@ -3,9 +3,9 @@
 
 value = input("Plese write here your string: ")
 
-#for i in value:
-#    if i.isupper():
-#        print(i)
+for i in value:
+    if i.isupper():
+        print(i)
 
 ###############################################
 # Друкує індекси усіх пробілів, якщо вони є, у прикладі вище це  1, 8
@@ -18,11 +18,33 @@ for index, letter in enumerate(value):
 
 ###############################################
 # Друкує усі гласні букви з рядка, у прикладі вище це  aAa
-pass
+
+list_words = ['A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y']
+answer = ''
+
+for i in value:
+    for letter in list_words:
+        if letter == i:
+            answer += letter
+print(answer)
 
 ###############################################
 # Якщо у рядку буде послідовність з трьох цифр, наприклад ab-c474f g - цикл переривається та друкується повідомлення про цю подію. Інакше друкується повідомлення про коректне завершення циклу
 
-pass
+counter = 0
+answer = ''
+
+
+for i in value:
+    if i.isalpha():
+        counter = 0
+        answer = ''
+    else:
+        counter += 1
+        answer += i
+        if len(answer) == 3:
+            break
+
+print(answer)
 
 ###############################################
